2de3baf569c874b27779493eab84468349b75e2c
Sahilpanchal2903 <sahilpanchal2903@gmail.com>
2022-03-13 16:24:42 +0000

    test

--- src/test/java/com/fthlbot/discordbotfthl/DiscordBotFthlApplicationTests.java
+++ src/test/java/com/fthlbot/discordbotfthl/DiscordBotFthlApplicationTests.java
@@ -1,13 +1,23 @@
 package com.fthlbot.discordbotfthl;
 
+import com.fthlbot.discordbotfthl.Util.GeneralService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import javax.xml.crypto.Data;
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 class DiscordBotFthlApplicationTests {
 
     @Test
-    void contextLoads() {
+    void isTodayBetweenStartDateAndEndDateForF8() throws ParseException, IOException {
+        boolean f8 = GeneralService.isValidDate("f8");
+        assertThat(f8).isEqualTo(true);
     }
 
 }
