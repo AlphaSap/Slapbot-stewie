3f4cd025af85e87176e9a4d5a0b277b85c58096d
Sahilpanchal2903 <sahilpanchal2903@gmail.com>
2022-03-18 23:40:25 +0000

    Exception when a team does not have enough roster changes left

---
+++ src/main/java/com/fthlbot/discordbotfthl/DatabaseModels/Exception/NoMoreRosterChangesLeftException.java
@@ -0,0 +1,7 @@
+package com.fthlbot.discordbotfthl.DatabaseModels.Exception;
+
+public class NoMoreRosterChangesLeftException extends LeagueException {
+    public NoMoreRosterChangesLeftException(String message) {
+        super(message);
+    }
+}
