715977593a15f5059bb7c4b9007704cbe1c2a1c4
Sahilpanchal2903 <sahilpanchal2903@gmail.com>
2022-04-09 10:29:00 +0100

    A command to add division week for each TH, this command can take a link to a discord message or just the raw JSON that can be entered via the command.

--- src/main/java/com/fthlbot/discordbotfthl/Commands/CommandImpl/AddDivisionWeeksImpl.java
+++ src/main/java/com/fthlbot/discordbotfthl/Commands/CommandImpl/AddDivisionWeeksImpl.java
@@ -10,19 +10,27 @@ import com.fthlbot.discordbotfthl.DatabaseModels.ScheduleWar.DivisonWeek.Divisio
 import com.fthlbot.discordbotfthl.DatabaseModels.ScheduleWar.DivisonWeek.DivisionWeeks;
 import com.fthlbot.discordbotfthl.Util.BotConfig;
 import com.fthlbot.discordbotfthl.Util.GeneralService;
+import org.javacord.api.entity.message.Message;
+import org.javacord.api.entity.message.MessageAttachment;
 import org.javacord.api.event.interaction.SlashCommandCreateEvent;
+import org.javacord.api.interaction.SlashCommandInteractionOption;
+import org.javacord.api.interaction.callback.InteractionOriginalResponseUpdater;
+import org.javacord.api.util.DiscordRegexPattern;
 import org.json.JSONArray;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
-import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Component
 @Invoker(
@@ -36,6 +44,7 @@ public class AddDivisionWeeksImpl implements AddDivisionWeekListener {
     private final DivisionWeekService divisionWeekService;
     private final BotConfig config;
     private final Logger log = LoggerFactory.getLogger(AddDivisionWeeksImpl.class);
+
     public AddDivisionWeeksImpl(DivisionService divisionService, DivisionWeekService divisionWeekService, BotConfig config) {
         this.divisionService = divisionService;
         this.divisionWeekService = divisionWeekService;
@@ -45,46 +54,85 @@ public class AddDivisionWeeksImpl implements AddDivisionWeekListener {
     @Override
     public void execute(SlashCommandCreateEvent event) {
         //TODO replace with user input
-        String json;
-        //TDOO regex for discord attachment link https://cdn.discordapp.com/attachments/[0-9]+/[0-9]+/[a-zA-Z\.]+
-        try {
-            json = GeneralService.getFileContent("sampleJSONforf5Week.json");
-        } catch (IOException e) {
-            e.printStackTrace();
-            return;
-        }
+        //TODO regex for discord attachment link https://cdn.discordapp.com/attachments/[0-9]+/[0-9]+/[a-zA-Z\.]+
+
+        List<SlashCommandInteractionOption> arguments = event.getSlashCommandInteraction().getArguments();
+        CompletableFuture<InteractionOriginalResponseUpdater> respondLater = event.getSlashCommandInteraction().respondLater();
+        String divAlias = arguments.get(0).getStringValue().get();
+        String json = arguments.get(1).getStringValue().get();
+        JSONArray s = null;
+
+        s = getJsonArray(event, respondLater, json);
+
+        if (s == null) return;
+
         Division division = null;
         try {
-            division = divisionService.getDivisionByAlias("f5");
+            division = divisionService.getDivisionByAlias(divAlias);
         } catch (EntityNotFoundException e) {
-            e.printStackTrace();
+            GeneralService.leagueSlashErrorMessage(respondLater, e);
+            return;
         }
-
-        JSONArray s = new JSONArray(json);
 
         int length = s.length();
         List<DivisionWeeks> divisionWeekList = new ArrayList<>();
+
         for (int i = 0; i < length; i++) {
             String start = s.getJSONObject(i).getString("start");
             Date date = null;
-            try {
-                date = new SimpleDateFormat("dd-MM-yyyy").parse(start);
-            } catch (ParseException e) {
-                e.printStackTrace();
-            }
 
             String end = s.getJSONObject(i).getString("end");
             boolean isByeWeek = s.getJSONObject(i).getBoolean("byeWeek");
             Date date1 = null;
             try {
+                date = new SimpleDateFormat("dd-MM-yyyy").parse(start);
                 date1 = new SimpleDateFormat("dd-MM-yyyy").parse(end);
             } catch (ParseException e) {
                 e.printStackTrace();
+                log.error(e.getMessage());
+                continue;
             }
 
-            DivisionWeeks divisionWeeks = new DivisionWeeks(i + 1, date, date1, division, isByeWeek );
-
+            DivisionWeeks divisionWeek = new DivisionWeeks(i + 1, date, date1, division, isByeWeek);
+            divisionWeekList.add(divisionWeek);
         }
         divisionWeekService.save(divisionWeekList);
+        respondLater.thenAccept(res -> {
+            res.setContent("Weeks added successfully").update();
+        });
+    }
+
+    private JSONArray getJsonArray(SlashCommandCreateEvent event, CompletableFuture<InteractionOriginalResponseUpdater> respondLater, String json) {
+        JSONArray s;
+        Pattern messageLink = DiscordRegexPattern.MESSAGE_LINK;
+
+        Matcher matcher = messageLink.matcher(json);
+        if (matcher.matches()) {
+            Optional<CompletableFuture<Message>> messageByLink = event.getApi().getMessageByLink(json);
+            if (messageByLink.isPresent()) {
+                Message message = messageByLink.get().join();
+                List<MessageAttachment> attachments = message.getAttachments();
+
+                if (attachments.size() >= 1) {
+                    byte[] join = attachments.get(0).downloadAsByteArray().join();
+                    String s1 = new String(join, StandardCharsets.UTF_8);
+                    s = new JSONArray(s1);
+                    System.out.println(s1);
+                } else {
+                    respondLater.thenAccept(res -> {
+                        res.setContent("No file found!").update();
+                    });
+                    return null;
+                }
+            } else {
+                respondLater.thenAccept(res -> {
+                    res.setContent("Unable to find the message, if the bot cannot see the message it cannot access it's content.").update();
+                });
+                return null;
+            }
+        }else{
+            s = new JSONArray(json);
+        }
+        return s;
     }
 }
