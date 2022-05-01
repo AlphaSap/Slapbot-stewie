67818bad7e42ffc6585545297abe4a92434ba9d1
Sahilpanchal2903 <sahilpanchal2903@gmail.com>
2022-05-01 10:49:27 +0100

    forgor ``!`

--- src/main/java/com/fthlbot/discordbotfthl/Handlers/CommandListener.java
+++ src/main/java/com/fthlbot/discordbotfthl/Handlers/CommandListener.java
@@ -59,7 +59,7 @@ public class CommandListener implements SlashCommandCreateListener {
                     event.getSlashCommandInteraction().createImmediateResponder().setContent("Sorry I do not work in DMs!").respond();
                     return;
                 }
-                if (canUserAnywhere(command, event.getSlashCommandInteraction().getServer().get())){
+                if (!canUserAnywhere(command, event.getSlashCommandInteraction().getServer().get())){
                     event.getSlashCommandInteraction().createImmediateResponder().setContent("Sorry this is not the correct the server to use this command!\nPlease see my help page to find where you can use this command!").respond();
                     return;
                 }
