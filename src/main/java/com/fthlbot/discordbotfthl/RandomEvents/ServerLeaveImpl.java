93170c6d946dee09cda50c9572dc47e79cb83b20
Sahilpanchal2903 <sahilpanchal2903@gmail.com>
2022-05-04 16:54:54 +0100

    two new events L

---
+++ src/main/java/com/fthlbot/discordbotfthl/RandomEvents/ServerLeaveImpl.java
@@ -0,0 +1,11 @@
+package com.fthlbot.discordbotfthl.RandomEvents;
+
+import org.javacord.api.event.server.ServerLeaveEvent;
+import org.javacord.api.listener.server.ServerLeaveListener;
+
+public class ServerLeaveImpl implements ServerLeaveListener {
+    @Override
+    public void onServerLeave(ServerLeaveEvent event) {
+
+    }
+}
