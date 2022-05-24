2b40f7edcbf138586fee0d1f4d8b175c0b5a23e6
Sahilpanchal2903 <sahilpanchal2903@gmail.com>
2022-05-24 19:50:17 +0100

    fix import

--- src/main/java/com/fthlbot/discordbotfthl/Commands/CommandListener/ClashCommandListener/ClanInfoListener.java
+++ src/main/java/com/fthlbot/discordbotfthl/Commands/CommandListener/ClashCommandListener/ClanInfoListener.java
@@ -1,6 +1,7 @@
 package com.fthlbot.discordbotfthl.Commands.CommandListener.ClashCommandListener;
 
-import com.fthlbot.discordbotfthl.Util.Handlers.Command;
+
+import com.fthlbot.discordbotfthl.Handlers.Command;
 
 public interface ClanInfoListener extends Command {
 }
