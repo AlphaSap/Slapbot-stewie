8718a2a0bdef4dafcce727dc464f7767f5fd5526
Sahilpanchal2903 <sahilpanchal2903@gmail.com>
2022-05-26 10:16:49 +0100

    Update: buttons will be now disabled and not removed, except for Pagination button

--- src/main/java/com/fthlbot/discordbotfthl/Util/Pagination/ButtonRemoveJobScheduler.java
+++ src/main/java/com/fthlbot/discordbotfthl/Util/Pagination/ButtonRemoveJobScheduler.java
@@ -8,7 +8,7 @@ import org.quartz.impl.StdSchedulerFactory;
 import java.util.Date;
 
 public class ButtonRemoveJobScheduler {
-    private static final long ONE_MINUTE_IN_MILLISECONDS = 60000;
+    private static final long ONE_MINUTE_IN_MILLISECONDS = 6000;
     public void execute(Object message) throws SchedulerException {
         JobDataMap dataMap = new JobDataMap();
         dataMap.put("message", message);
