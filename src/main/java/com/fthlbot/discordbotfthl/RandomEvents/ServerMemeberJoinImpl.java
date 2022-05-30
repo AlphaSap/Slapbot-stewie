7c2fb91bbe9d7107c9287e6fde7c633fff2e7914
SpooKy <spookyhammer2300@gmail.com>
2022-05-30 16:29:27 +0530

    removed unwanted import

--- src/main/java/com/fthlbot/discordbotfthl/RandomEvents/ServerMemeberJoinImpl.java
+++ src/main/java/com/fthlbot/discordbotfthl/RandomEvents/ServerMemeberJoinImpl.java
@@ -16,7 +16,6 @@ import org.springframework.stereotype.Component;
 
 import java.awt.*;
 import java.util.List;
-import java.util.Optional;
 
 @Component
 public class ServerMemeberJoinImpl implements ServerMemberJoinListener {
