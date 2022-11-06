2c961bc7a21b086ec74b5b512ad373821a89ed13
SaHHiiLL <sahilpanchal2903@gmail.com>
2022-11-06 13:13:56 +0000

    never mind lolz

--- src/main/java/com/fthlbot/discordbotfthl/Commands/CommandImpl/StaffCommandsImpl/InfoCommandImpl.java
+++ src/main/java/com/fthlbot/discordbotfthl/Commands/CommandImpl/StaffCommandsImpl/InfoCommandImpl.java
@@ -1,8 +1,7 @@
 package com.fthlbot.discordbotfthl.Commands.CommandImpl.StaffCommandsImpl;
 
-import com.fthlbot.discordbotfthl.Util.Pagination.Pagination;
-import com.fthlbot.discordbotfthl.core.Annotation.Invoker;
 import com.fthlbot.discordbotfthl.DatabaseModels.Team.TeamService;
+import com.fthlbot.discordbotfthl.core.Annotation.Invoker;
 import com.fthlbot.discordbotfthl.core.Handlers.Command;
 import org.javacord.api.entity.message.embed.EmbedBuilder;
 import org.javacord.api.entity.server.Server;
@@ -12,8 +11,6 @@ import org.springframework.stereotype.Component;
 
 import java.awt.*;
 import java.util.Collection;
-import java.util.List;
-import java.util.Objects;
 import java.util.concurrent.CompletableFuture;
 
 @Component
@@ -41,7 +38,7 @@ public class InfoCommandImpl implements Command {
                 .addInlineField("User count", members + "")
                 .addInlineField("Command count", "TODO")
                 .addInlineField("Discord Wrapper", "Javacord")
-                .addInlineField("Library Version", "3.5.0")
+                .addInlineField("Library Version", "3.6.0")
                 .addInlineField("Bot Version", "2.0.0")
                 .addInlineField("Bot Author", event.getApi().getOwner().get().join().getDiscriminatedName())
                 .addInlineField("Teams Registered in FTHL", teamService.getAllTeams().size()+  "")
@@ -49,27 +46,6 @@ public class InfoCommandImpl implements Command {
                 .setFooter("1/2")
                 .setTimestampToNow();
 
-        Pagination pagination = new Pagination();
-
-        pagination.buttonPagination(List.of(em, getSystemInfo()), res, event.getApi());
-    }
-
-    /**
-     * @return an embed with system information
-     */
-    private EmbedBuilder getSystemInfo() {
-        String file = Objects.requireNonNull(this.getClass().getResource("ubuntu.png")).getFile();
-        assert file != null;
-
-        return new EmbedBuilder()
-                .setTitle("Host information")
-                .addField("OS", System.getenv("os.name"))
-                .addField("Version", System.getenv("os.version"))
-                .addField("Architecture", System.getProperty("os.arch"))
-                .addField("Java Version", "java.version")
-                .setColor(Color.green)
-                .setTimestampToNow()
-                .setFooter("2/2")
-                .setThumbnail(file);
+        res.thenAccept(r -> r.addEmbed(em).update());
     }
 }
