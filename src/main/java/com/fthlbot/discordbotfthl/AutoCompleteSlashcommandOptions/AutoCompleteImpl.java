429f501b8d02e8cfd0cba2aa157d20cfbaa6d149
SaHHiiLL <sahilpanchal2903@gmail.com>
2022-11-20 17:19:11 +0000

    test #3 for auto complete

--- src/main/java/com/fthlbot/discordbotfthl/AutoCompleteSlashcommandOptions/AutoCompleteImpl.java
+++ src/main/java/com/fthlbot/discordbotfthl/AutoCompleteSlashcommandOptions/AutoCompleteImpl.java
@@ -1,11 +1,15 @@
 package com.fthlbot.discordbotfthl.AutoCompleteSlashcommandOptions;
 
 import com.fthlbot.discordbotfthl.DatabaseModels.Division.Division;
+import com.fthlbot.discordbotfthl.DatabaseModels.Division.DivisionService;
+import com.fthlbot.discordbotfthl.DatabaseModels.Exception.EntityNotFoundException;
 import com.fthlbot.discordbotfthl.DatabaseModels.Team.Team;
 import com.fthlbot.discordbotfthl.DatabaseModels.Team.TeamService;
 import org.javacord.api.event.interaction.AutocompleteCreateEvent;
 import org.javacord.api.interaction.SlashCommandOptionChoice;
 import org.javacord.api.listener.interaction.AutocompleteCreateListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -15,9 +19,12 @@ import java.util.stream.Collectors;
 @Component
 public class AutoCompleteImpl implements AutocompleteCreateListener {
     private final TeamService teamService;
+    private final DivisionService divisionService;
 
-    public AutoCompleteImpl(TeamService teamService) {
+    private final Logger log = LoggerFactory.getLogger(AutoCompleteImpl.class);
+    public AutoCompleteImpl(TeamService teamService, DivisionService divisionService) {
         this.teamService = teamService;
+        this.divisionService = divisionService;
     }
 
     @Override
@@ -32,8 +39,18 @@ public class AutoCompleteImpl implements AutocompleteCreateListener {
            System.out.println("No input yet!");
            return;
        }
+       Division division = null;
+       var div = event.getAutocompleteInteraction().getArgumentStringValueByName("division");
+       if (div.isPresent()) {
+           try {
+               division = divisionService.getDivisionByAlias(div.get());
+           } catch (EntityNotFoundException e) {
+                log.error("Cannot get division for auto complete!, must be empty!");
+                e.printStackTrace();
+           }
+       }
 
-       List<String> teams = getTeamNames(stringValue.get(), null);
+       List<String> teams = getTeamNames(stringValue.get(), division);
         System.out.println(teams);
        List<SlashCommandOptionChoice> options = parseOptionFromListOfString(teams);
        event.getAutocompleteInteraction().respondWithChoices(options);
