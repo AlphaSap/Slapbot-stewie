e836610aa0ea17ce7a58162742b1c98c1e012ab0
sahil <sahilpanchal2903@gmail.com>
2023-10-09 14:37:17 +0100

    Final update for today I think

--- src/main/java/com/fthlbot/discordbotfthl/Events/ServerMemberJoin/ApplicantServerJoinImpl.java
+++ src/main/java/com/fthlbot/discordbotfthl/Events/ServerMemberJoin/ApplicantServerJoinImpl.java
@@ -35,13 +35,19 @@ public class ApplicantServerJoinImpl implements ServerMemberJoinListener {
                 .setUser(event.getUser())
                 .setTeams(teamByRep);
 
-        EmbedBuilder embedBuilder = serverMemberJoinService.getEmbed();
+        String message = "Welcome! <:%d> \n" +
+                "Please read the rules and then checkout <#%d> to apply for the tournament. \n" +
+                "If you have any questions, please ask in <#%d>.".formatted(
+                        event.getUser().getId(),
+                        984432320511606876L,
+                        926324986078175283L);
+//        EmbedBuilder embedBuilder = serverMemberJoinService.getEmbed();
 
         TextChannel sysChannel = event.getServer().getSystemChannel().orElse(
                 event.getServer().getTextChannels().get(0)
         ); // will return the first channel in case the system channel is not set
 
-        sysChannel.sendMessage(embedBuilder);
+        sysChannel.sendMessage(message);
 
         serverMemberJoinService.giveRoles(event.getServer(), botConfig.getApplicantServerApplicantRoleID(), "Applicant");
     }
